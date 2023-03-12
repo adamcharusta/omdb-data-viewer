434a20bd65afe4d59baede03d4089042ce5812fd
adamcharusta <dQa!n26H4u>
2023-03-13 00:55:12 +0100

    remove transloco bugs, remove unnecessary tests

--- src/app/services/mat-paginator-intl-cro.service.ts
+++ src/app/services/mat-paginator-intl-cro.service.ts
@@ -1,12 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { MatPaginatorIntl } from '@angular/material/paginator';
 import { TranslocoService } from '@ngneat/transloco';
 import { CapitalizePipe } from '../pipes/capitalize.pipe';
+import { Subject, takeUntil } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class MatPaginatorIntlCroService extends MatPaginatorIntl {
+export class MatPaginatorIntlCroService
+  extends MatPaginatorIntl
+  implements OnDestroy
+{
+  destroy$: Subject<boolean> = new Subject<boolean>();
   private capitalize = new CapitalizePipe();
 
   override firstPageLabel = this.capitalize.transform(
@@ -22,28 +27,52 @@ export class MatPaginatorIntlCroService extends MatPaginatorIntl {
     this.translocoService.translate('paginator.previous-page')
   );
 
+  private paginatorRange = this.translocoService.translate('paginator.range');
+
   constructor(private translocoService: TranslocoService) {
     super();
+
+    this.translocoService.langChanges$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => {
+        this.nextPageLabel = this.capitalize.transform(
+          this.translocoService.translate('paginator.next-page')
+        );
+
+        this.firstPageLabel = this.capitalize.transform(
+          this.translocoService.translate('paginator.first-page')
+        );
+
+        this.lastPageLabel = this.capitalize.transform(
+          this.translocoService.translate('paginator.last-page')
+        );
+
+        this.previousPageLabel = this.capitalize.transform(
+          this.translocoService.translate('paginator.previous-page')
+        );
+
+        this.paginatorRange =
+          this.translocoService.translate('paginator.range');
+      });
   }
   override getRangeLabel = (page: number, pageSize: number, length: number) => {
     if (length === 0 || pageSize === 0) {
-      return '0 od ' + length;
+      return `0 ${this.paginatorRange}` + length;
     }
 
     length = Math.max(length, 0);
     const startIndex = page * pageSize;
-    // If the start index exceeds the list length, do not try and fix the end index to the end.
     const endIndex =
       startIndex < length
         ? Math.min(startIndex + pageSize, length)
         : startIndex + pageSize;
     return (
-      startIndex +
-      1 +
-      ' - ' +
-      endIndex +
-      ` ${this.translocoService.translate('paginator.range')} ` +
-      length
+      startIndex + 1 + ' - ' + endIndex + ` ${this.paginatorRange} ` + length
     );
   };
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
 }
